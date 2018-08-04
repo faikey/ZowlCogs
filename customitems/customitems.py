56e938a946859a8c9312a96d3eec3a785e7bbc25
Zylvian <jovlisen@gmail.com>
2018-08-04 09:24:45 +0200

    Fixed dict integration in customitems
    Easily configurable item keys

--- customitems/customitems.py
+++ customitems/customitems.py
@@ -7,22 +7,20 @@ import random
 class CustomItems:
     """My custom cog"""
     
-    items = {"Gold Bar": 100, "Safe": {"Blue": 0.1, "Yellow": 0.2, "Red": 0.3}}
+     
     
     def __init__(self):
-        self.config = Config.get_conf(self, identifier=5074395003, force_registration=True)
+        self.config = Config.get_conf(self, identifier=8358350001, force_registration=True)
         
-        items = {"Gold Bar": 100, "Safe": {"Blue": 0.1, "Yellow": 0.2, "Red": 0.3}}
+        items = {"items": {"Gold_Bar": 100, "Safe": {"Blue": 0.1, "Yellow": 0.2, "Red": 0.3}}}
         
-        self.config.register_guild(**items
+        self.config.register_guild(**items)
         
-    
-    @commands.command()
-    async def mycom(self, ctx):
-        # Your code will go here
-        await ctx.send("I can do stuff!")
+        
 
     async def redeem_item(self, ctx, item):
+        self.gconf = self.config.guild(ctx.guild)
+        
         if(item == 'Gold Bar'):
             await self.redeem_goldbar(ctx, item)
         elif('Safe' in item):
@@ -31,6 +29,8 @@ class CustomItems:
             ctx.send("Sorry, can't redeem that item!")
     
     async def redeem_goldbar(self, ctx, item):
+        self.gconf = self.config.guild(ctx.guild)
+        
         shop = ctx.bot.get_cog('Shop')
         inventory = await shop.inv_hook(ctx.author)
         author = ctx.author
@@ -42,7 +42,7 @@ class CustomItems:
                 await shop.item_remove(ctx, 'Gold Bar')
                 currency = await bank.get_currency_name(ctx.guild)
                 bal = await bank.get_balance(author)
-                goldworth = 100
+                goldworth = await self.gconf.get_raw('items', 'Gold_Bar')
                 newbal = bal + goldworth
                 
                 await bank.set_balance(author, newbal)
@@ -52,21 +52,24 @@ class CustomItems:
         except KeyError:
             await ctx.send("You don't have a gold bar!")
             
-    async def redeem_safe(self, ctx, item):  
+    async def redeem_safe(self, ctx, item): 
+        
+        safestr = " Safe"
+        self.gconf = self.config.guild(ctx.guild)
         user = ctx.author
         rob = ctx.bot.get_cog('Rob')
         shop = ctx.bot.get_cog('Shop')
-        safes = ('Blue', 'Yellow', 'Red')
+       
+        # Gets the safe color, uses it to find the safe value, increases user's Rob Defense.
+        safe_colors = await self.gconf.get_raw('items', 'Safe')
+        item_color = item.split(' ')[0]
+        if(item_color not in safe_colors):
+            return await ctx.send("That safe color doesn't exist here! Scream, weirdo!")
         
-        await ctx.send(safes[0])
+        #Increases rob defense.
+        value = safe_colors[item_color]
+        await rob.rob_def_increase(ctx, value)
         
-        if(safes[0] in item):
-            await rob.rob_def_increase(ctx, 0.1)
-        if(safes[0] in item):
-            await rob.rob_def_increase(ctx, 0.2)
-        if(safes[0] in item):
-            await rob.rob_def_increase(ctx, 0.3)
-            
         await shop.item_remove(ctx, item)
             
     

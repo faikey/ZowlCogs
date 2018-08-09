3c6ec25739e56a05c4f500158db277e1aa5b55dd
qwazwsx <azimbin@gmail.com>
2018-08-09 02:05:06 -0500

    c

--- Leaderboard/leaderboard.py
+++ Leaderboard/leaderboard.py
@@ -21,7 +21,7 @@ class Leaderboard:
     @commands.command()
     #@commands.cooldown(rate=1, per=14400, type=discord.ext.commands.BucketType.user)
     async def ll(self, ctx):
-        from economy import Economy
-        Economy._server_leaderboard(ctx, 10)
+        economy = ctx.bot.get_cog('economy') #its actually just bank you named the test cog wrong
+        economy.leaderboard_pass(ctx, 10, True)
 
     

189a38012c6d7e6c7a9cfc3be2c1656f6dd80f55
qwazwsx <azimbin@gmail.com>
2018-08-11 01:23:05 -0500

    a

--- Leaderboard/leaderboard.py
+++ Leaderboard/leaderboard.py
@@ -21,7 +21,7 @@ class Leaderboard:
         channel = self.bot.get_channel(474332690381013002)
 
         async for message in channel.history(limit=5):
-            print(type message.author.id)
+            print(type(message.author.id))
             print(message.author.id == '474030873742671892')
             print(message.author.id is '474030873742671892')
 

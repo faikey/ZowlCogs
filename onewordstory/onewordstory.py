0a53ad209577f5b7110ecb51e701a4b336222a7a
Zylvian <jovlisen@gmail.com>
2018-08-30 22:24:28 +0200

    1.5 hours

--- onewordstory/onewordstory.py
+++ onewordstory/onewordstory.py
@@ -53,7 +53,7 @@ class OneWordStory:
         
      
      
-        ows_defaults = {'Cooldown': 7200,
+        ows_defaults = {'Cooldown': 5400,
                             'Counter': 0,
                             'Round_time': 100,
                             'Start_time': 60,
